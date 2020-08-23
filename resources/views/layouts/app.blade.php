a2940bc08f5dec6580882e978ce2f5aa9b50ede5
asano.watanabe <asano.w@p-tech.co.jp>
2020-08-23 11:18:17 +0000

    フッターを殺した

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 
             @yield('content')
         </div>
-        
+        <!--
        <footer class="footer" style="width:100%;  
                         position: fixed;
                     bottom: 0; /*下に固定*/
@@ -26,7 +26,7 @@
                     background-color: #212529; color:white; text-align: center;">
               <small>        &copy;2020 Asano kensyuu-de-tukuttayatu       </small>
         </footer>
-            
+            -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
